852ac66a125610ddeaf8e25447563411c9c3038c
tebr1z <tabrizyh@code.edu.az>
2024-03-11 16:50:09 +0400

    Feat : User

--- hometask_InterfaceAbstraction/Auth/User.cs
+++ hometask_InterfaceAbstraction/Auth/User.cs
@@ -6,7 +6,98 @@ using System.Threading.Tasks;
 
 namespace hometask_InterfaceAbstraction.Auth
 {
-    internal class User
+ 
+public class User : IAccount
+{
+    public int Id { get; }
+    public string Fullname { get; }
+    public string Email { get; }
+    private string Password { get; }
+
+    public User(int id, string fullname, string email, string password)
     {
+        Id = id;
+        Fullname = fullname;
+        Email = email;
+
+        while (!PasswordChecker(password))
+        {
+            Console.WriteLine("Sifre duzgun deyil tekrar cehd edin");
+            Console.Write("Password daxil edin: ");
+            password = Console.ReadLine();
+        }
+        Password = password;
     }
+
+    public bool PasswordChecker(string password)
+    {
+        if (password.Length < 8)
+        {
+           
+            Console.WriteLine("Sifre en az 8 reqem ve herf olmalidir.");
+            return false;
+        }
+        if (!ContainsUpperCase(password))
+        {
+          
+            Console.WriteLine("Sifre en az bir boyuk herf olmalidir");
+            return false;
+        }
+        if (!ContainsLowerCase(password))
+        {
+         
+            Console.WriteLine("Şifre en az  bir balaca herf olmalidir");
+            return false;
+        }
+        if (!ContainsDigit(password))
+        {
+            
+            Console.WriteLine("Sifre en bir reqem olamlidir");
+            return false;
+        }
+
+        return true;
+    }
+
+    private bool ContainsUpperCase(string password)
+    {
+        foreach (char c in password)
+        {
+            if (char.IsUpper(c))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private bool ContainsLowerCase(string password)
+    {
+        foreach (char c in password)
+        {
+            if (char.IsLower(c))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private bool ContainsDigit(string password)
+    {
+        foreach (char c in password)
+        {
+            if (char.IsDigit(c))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"Id: {Id}, Fullname: {Fullname}, Email: {Email}");
+    }
+}
 }
